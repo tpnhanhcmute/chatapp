074591c76fe949c62b65a7a0aede2bbd8bae6a0f
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-04 16:21:12 +0700

    add onclick register

--- app/src/main/java/com/example/chatapp/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/chatapp/Activity/RegisterActivity.java
@@ -1,13 +1,27 @@
 package com.example.chatapp.Activity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.chatapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button btnRegister;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
