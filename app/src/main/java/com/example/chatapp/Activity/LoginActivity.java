5e5d6ce82c7a096bc58344d1ae312edc1467ac05
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-04 15:49:37 +0700

    add

---
+++ app/src/main/java/com/example/chatapp/Activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatapp.R;
+
+public class LoginActivity  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/Activity/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.chatapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.chatapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
