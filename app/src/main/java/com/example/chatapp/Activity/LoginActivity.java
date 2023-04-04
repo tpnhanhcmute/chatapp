06b0e9e3f3526b848ed0f4e8a13de5ef0ddd5375
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-04 16:53:06 +0700

    add action trans activity

--- app/src/main/java/com/example/chatapp/Activity/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/Activity/LoginActivity.java
@@ -1,14 +1,56 @@
 package com.example.chatapp.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chatapp.R;
 
 public class LoginActivity  extends AppCompatActivity {
+    ImageView imageViewBack;
+    Button buttonLogin;
+    EditText editTextPhone;
+    TextView textViewRegister;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Mapping();
+        SetListener();
+    }
+
+    private void SetListener() {
+        imageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.super.onBackPressed();
+            }
+        });
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void Mapping() {
+        imageViewBack = findViewById(R.id.imageViewBack);
+        buttonLogin = findViewById((R.id.buttonLogin));
+        editTextPhone= findViewById(R.id.editTextPhone);
+        textViewRegister = findViewById(R.id.textViewRegister);
     }
 }
--- app/src/main/java/com/example/chatapp/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/chatapp/Activity/RegisterActivity.java
@@ -6,20 +6,56 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.chatapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
+    ImageView imageViewBack;
+    EditText editTextName;
+    EditText editTextEmail;
+    EditText editTextPhone;
+    TextView textViewLogin;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Mapping();
+        SetListener();
+    }
+    private  void Mapping(){
         btnRegister = findViewById(R.id.btnRegister);
+        imageViewBack = findViewById(R.id.imageViewBack);
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        textViewLogin = findViewById(R.id.textViewLogin);
+    }
+    private  void SetListener(){
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        imageViewBack.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                RegisterActivity.super.onBackPressed();
+            }
+        });
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
