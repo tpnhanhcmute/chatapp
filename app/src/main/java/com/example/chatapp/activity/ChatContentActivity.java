76e84afdb791bc95d0e620efa0d0b7057e6f1c61
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-05 17:15:56 +0700

    fix ui chat

--- app/src/main/java/com/example/chatapp/activity/ChatContentActivity.java
+++ app/src/main/java/com/example/chatapp/activity/ChatContentActivity.java
@@ -2,6 +2,7 @@ package com.example.chatapp.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,18 +20,19 @@ public class ChatContentActivity extends AppCompatActivity {
     private ImageView imageViewBack;
     private MessageAdapter messageAdapter;
     private RecyclerView rcMessage;
+    private EditText editTextMessage;
+    List<Message> messageList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_content);
         Mapping();
-        SetListener();
         LoadData();
-
+        SetListener();
 
     }
     private  void LoadData(){
-        List<Message> messageList = new ArrayList<Message>();
+        messageList = new ArrayList<Message>();
         Message message = new Message();
         Message message1 = new Message();
         message.sender ="nhan";
@@ -91,12 +93,14 @@ public class ChatContentActivity extends AppCompatActivity {
                         false);
         rcMessage.setLayoutManager(layoutManager);
         rcMessage.setAdapter(messageAdapter);
+        rcMessage.scrollToPosition(messageList.size()-1);
         messageAdapter.notifyDataSetChanged();
 
     }
     private  void Mapping(){
         imageViewBack= (ImageView)findViewById(R.id.imageViewBack);
         rcMessage = (RecyclerView) findViewById(R.id.recycleViewMessage);
+        editTextMessage = findViewById(R.id.editTextMessage);
     }
     private  void SetListener(){
         imageViewBack.setOnClickListener(new View.OnClickListener() {
